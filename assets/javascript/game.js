adee842de54d6ed87a0f6211ef0d913ec23bc134
kamoltip <plawansusu@gmail.com>
2017-05-20 04:25:23 -0400

    function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 //psuedo code
 
-//function the random no. onload.
+//function the random no. 
 //set value in each one of the crystal.
 // ..hide value amount.
 // ..when click , update the score counter.
+///////////////////score wise/////////////////////
 //condition..
 // .. player wins if total score matches the random no.
 // .. player loses if their > randomNo.
@@ -34,15 +35,88 @@ $("#ruleContent").slideUp(1500);
 });
 
 
-//generate random No. onload
-window.onload = getNumber; //
+//generate random No.//
 
+	var randomnumber;
 function getNumber() {
-    var minNumber = 20; // The minimum number you want
-    var maxNumber = 90; // The maximum number you want
-    var randomnumber = Math.floor(Math.random() * (maxNumber) + minNumber)+1; // Generates random number
+    var minNumber = 20; 
+    var maxNumber = 90; 
+    randomnumber = Math.floor(Math.random() * (maxNumber) + minNumber)+1; // Generates random number
     $('#randomNo').html(randomnumber); // Sets content of <div> to number
-    return false; // Returns false just to tidy everything up
+    console.log(randomnumber);
+    return randomnumber;
+
 }
 
-	$("#randomNo").prepend(getNumber);
+getNumber();
+
+//set value to the crystals//
+// var blue;
+// var green;
+// var red;
+// var yellow;
+
+
+//scoring for blue crystal//
+
+var yourScore = 0;
+var scoreCounter;
+	
+function crystal(id){
+	scoreCounter = $("#totalScore")
+	var crystalMin = 1; 
+    var crystalMax = 11; 
+    blueRandom = Math.floor(Math.random() * (crystalMax) + crystalMin)+1; 
+    $(id).attr('value', blueRandom);
+
+    return blueRandom;
+}
+crystal("#blue");
+crystal("#green");
+crystal("#red");
+crystal("#yellow");  
+
+var yourScore;
+var value;
+function getValue() {
+	$(".value").on("click", function () {
+		value = $(this).attr("value");
+		console.log(typeof parseInt(value));
+		yourScore += parseInt(value);
+		console.log(yourScore);
+		$("#totalScore").html(yourScore);
+		
+		var loseCounter=0;
+		if(yourScore > randomnumber){
+			loseCounter++;
+			$("#loseCounter").html(loseCounter);
+		} 
+		var winCounter=0;
+		if(yourScore === randomnumber){
+			winCounter++;
+			$("#winCounter").html(winCounter);
+		}
+		if(winCounter === 1){
+			$("#gameResult").html("YOU WIN!!!")
+		}
+		if(loseCounter === 1){
+			$("#gameResult").html("YOU LOSE!!!")
+		}
+});
+
+}
+getValue();
+
+	
+
+
+	
+
+	
+	
+
+
+
+
+
+
